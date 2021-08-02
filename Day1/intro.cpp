b629e9a3d410ec61ae4d9fe6690f530619f9ecea
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-02 14:22:02 +0530

    first commit

---
+++ Day1/intro.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Write C++ code here
+    cout << "Hello world!";
+
+    return 0;
+}
