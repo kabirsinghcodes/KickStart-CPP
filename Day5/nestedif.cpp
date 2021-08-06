1bf5bbd0f548f5f22babe777442a8c561ec9a3c9
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-06 16:40:51 +0530

    added

---
+++ Day5/nestedif.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int x = 5;
+    if (x < 6)
+    {
+        cout << "Hello";
+    }
+    if (x == 5)
+    {
+        cout << "Hi";
+    }
+    else
+    {
+        cout << "Hey";
+    }
+    return 0;
+}
