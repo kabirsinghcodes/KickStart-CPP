5ebe043fd8569612a1692ea7dd9d810aa65e7573
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-07 22:13:51 +0530

    added

---
+++ Day5/fun.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int a = 10, b = 20, c = 30;
+    if (a <= b && !b)
+    {
+        cout << "hello";
+    }
+    else if (c >= a && c >= b)
+    {
+        cout << "hi";
+    }
+    else
+    {
+        cout << "heyyaaaa";
+    }
+    return 0;
+}
