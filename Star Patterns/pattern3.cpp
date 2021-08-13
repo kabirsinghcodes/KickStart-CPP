6a1f0339c1cc12cac07d979d3680965d92ef122f
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-13 18:13:26 +0530

    added

---
+++ Star Patterns/pattern3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 5; j >= i; j--)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
