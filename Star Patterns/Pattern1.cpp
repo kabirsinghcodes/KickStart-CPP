d2daf684e661d9ffb825474293e62439d5fbb343
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-13 17:47:51 +0530

    added

---
+++ Star Patterns/Pattern1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Write C++ code here
+    int n;
+    cout << "Enter the number of rows : " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
