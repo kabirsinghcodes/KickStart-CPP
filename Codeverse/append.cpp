11371ed1d1e5bd1d5c14d3a0b95a5512a0a0971d
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-26 17:55:40 +0530

    added

---
+++ Codeverse/append.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1 = "Kabir";
+    string s2 = "Singh";
+    string s3 = s1 + s2;
+    cout << s3;
+    return 0;
+}
---
+++ Codeverse/stringsss.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter your string : ";
+    cin >> str;
+
+    cout << "string : " << str << endl;
+
+    return 0;
+}
