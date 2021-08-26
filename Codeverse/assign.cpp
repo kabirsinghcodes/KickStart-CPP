69a760c3d5c4b2492ed7a1a7c1118bab42bcc39b
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-26 18:06:35 +0530

    added

---
+++ Codeverse/assign.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name = "Kabeer";
+    name = "Kabir";
+    cout << name;
+    return 0;
+}
---
+++ Codeverse/at.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name = "Kabir";
+    cout << name.at(3) << endl;
+    return 0;
+}
