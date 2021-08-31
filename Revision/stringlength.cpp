152ba5597d21b1318f0171d6c77f6400b0edabd1
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-31 17:58:23 +0530

    added

---
+++ Revision/stringlength.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "kabir singh";
+    cout << str << endl;
+    cout << "Length of string is : " << str.size();
+    return 0;
+}
