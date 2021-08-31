612da154d7d949d95bf3e69091f76a8e1c605c5a
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-31 17:35:33 +0530

    added

---
+++ Revision/add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Enter two numbers : " << endl;
+    cin >> a >> b;
+    sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum;
+    return 0;
+}
